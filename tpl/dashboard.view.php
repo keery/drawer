501cbb6877e9a050eda3722f3c78850bf4f24b99
Alex <alexandre.charlot@wisembly.com>
2018-02-17 19:57:55 +0100

    landing

---
+++ tpl/dashboard.view.php
@@ -0,0 +1,55 @@
+<h1>Dashboard</h1>
+<div id="dashboard">
+  <div class="top">
+    <section>
+      <header>
+        <span>derniers articles</span>
+        <img src="" alt="">
+      </header>
+      <div class="content">
+        <div class="children">
+          <h3>Jean louis</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Sunt quo quae repellendus obcaecati maiores? Possimus aut ea mollitia voluptas, 
+          ullam quam labore obcaecati, in error ducimus illo expedita recusandae. Est.</p>
+          <span>il y a 2 jours</span>
+        </div>
+        <div class="children">
+          <h3>Jean louis</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Sunt quo quae repellendus obcaecati maiores? Possimus aut ea mollitia voluptas, 
+          ullam quam labore obcaecati, in error ducimus illo expedita recusandae. Est.</p>
+          <span>il y a 2 jours</span>
+        </div>
+      </div>
+    </section>
+    <section>
+      <header>
+        <span>derniers commentaires</span>
+        <img src="" alt="">
+      </header>
+      <div class="content">
+        <div class="children">
+          <h3>Jean louis</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Sunt quo quae repellendus obcaecati maiores? Possimus aut ea mollitia voluptas, 
+          ullam quam labore obcaecati, in error ducimus illo expedita recusandae. Est.</p>
+          <span>il y a 2 jours</span>
+        </div>
+        <div class="children">
+          <h3>Jean louis</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+          Sunt quo quae repellendus obcaecati maiores? Possimus aut ea mollitia voluptas, 
+          ullam quam labore obcaecati, in error ducimus illo expedita recusandae. Est.</p>
+          <span>il y a 2 jours</span>
+        </div>
+      </div>
+    </section>
+  </div>
+  <hr class="lineSeparator">
+  <div class="bottom">
+    <img src="assets/img/stats.svg" alt="">
+    <img src="assets/img/stats2.svg" alt="">
+    <span>TOTAL - 12 300 views</span>
+  </div>
+</div>
