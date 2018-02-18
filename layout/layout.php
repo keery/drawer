e8e73404db504424c072813dc4cb3001d31505c2
ESNAULT-Guillaume <mon-compte1131@live.fr>
2018-02-18 20:21:38 +0100

    Mise en place de la structure responsive

--- layout/layout.php
+++ layout/layout.php
@@ -35,5 +35,7 @@
 	<footer>
 		
 	</footer>
+    <script src="http://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
+    <script src="js/index.js"></script>	
 	</body>
 </html>
--- tpl/pages-list.view.php
+++ tpl/pages-list.view.php
@@ -12,23 +12,35 @@
     <li class="list-item">
       <div class="text-list">Mes dessins</div>
       <div class="details-list"><span class="date-details-list">1 month ago</span></div>
-      <a href="#" class="btn-edit-item"><span></span></a>
-    </li>
-    <li class="list-item">
-      <div class="text-list">Mes histoires</div>
-      <div class="details-list"><span class="date-details-list">2 months ago</span></div>
-      <a href="#" class="btn-edit-item"><span></span></a>
-    </li>
-    <li class="list-item">
-      <div class="text-list">Contactez-moi</div>
-      <div class="details-list"><span class="date-details-list">3 months ago</span></div>
-      <a href="#" class="btn-edit-item">
+      <span class="btn-edit-item">
         <span></span>
         <ul>
           <li><a href="">Éditer</a></li>
           <li><a href="">Supprimer</a></li>
         </ul>
-      </a>
+      </span>
+    </li>
+    <li class="list-item">
+      <div class="text-list">Mes histoires</div>
+      <div class="details-list"><span class="date-details-list">2 months ago</span></div>
+      <span class="btn-edit-item">
+        <span></span>
+        <ul>
+          <li><a href="">Éditer</a></li>
+          <li><a href="">Supprimer</a></li>
+        </ul>
+      </span>
+    </li>
+    <li class="list-item">
+      <div class="text-list">Contactez-moi</div>
+      <div class="details-list"><span class="date-details-list">3 months ago</span></div>
+      <span class="btn-edit-item">
+        <span></span>
+        <ul>
+          <li><a href="">Éditer</a></li>
+          <li><a href="">Supprimer</a></li>
+        </ul>
+      </span>
     </li>        
   </ul>
 </section>
