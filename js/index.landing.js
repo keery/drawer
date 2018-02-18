a56bd5ff0883fdeeefc0213debd12ef8e5a8fa83
ESNAULT-Guillaume <mon-compte1131@live.fr>
2018-02-18 12:54:16 +0100

    Module contact landing page et modification responsive

--- js/index.landing.js
+++ js/index.landing.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
 		if(nav.hasClass('open')) nav.removeClass('open');
 		else nav.addClass('open');
 	});
+
+	$(document).on("click", ".main-menu>li>a", function(){
+		if($("#burger").is(':visible'))
+		{
+			$('.nav-top').removeClass('open');
+		}
+	});
+
 });
--- layout/layout-landing.php
+++ layout/layout-landing.php
@@ -4,6 +4,7 @@
 		<link rel="stylesheet" href="assets/css/dist/style.css">
 		<meta charset="UTF-8">
 		<title>Creative Drawer</title>		
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
 	</head>
 	<body>
     <div id="landing">
@@ -12,16 +13,16 @@
         <nav class="nav-top row">
           <ul class="main-menu">
             <li>
-              <a href="#">Fonctionnalités</a>
+              <a href="#band">A propos</a>
             </li>
             <li>
-              <a href="#">Websites</a>
+              <a href="#creation">Fonctionnalités</a>
             </li>
             <li>
-              <a href="#">A propos</a>
+              <a href="#community">Communauté</a>
             </li>
             <li>
-              <a href="#">Contact</a>
+              <a href="#contact">Contact</a>
             </li>		
           </ul>
           <ul class="connect-option">
@@ -40,10 +41,10 @@
        <img src="assets/img/logo-creative-drawer.png" alt="Logo Creative Drawer" id="logo">
         <div class="skew-right">
           <div class="bloc-slider">
-            <h1>Ma grosse baseline</h1>
+            <h1>CMS Creative Drawer</h1>
             <div class="subtitle">Sous titre</div>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis tempore pariatur ipsum earum, aperiam cupiditate recusandae, quae debitis animi, optio nulla dolores. A molestiae tempora, assumenda asperiores itaque, officiis sint.</p>
-            <button class="btn">Get started</button>
+            <button class="btn">Débuter</button>
           </div>
         </div>
         <a href="#band" class="chevron-down" title="Descendre à la partie inférieur"></a>
@@ -53,11 +54,11 @@
         <div class="text-center text-band">
           <h2>Ma grosse baseline</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore facilis quae, optio, soluta eligendi nostrum, eveniet doloremque, asperiores tempora est deserunt delectus placeat. Voluptatibus eaque officia dolore, quidem rerum dolorem.</p>
-          <button class="btn op">Get started</button>
+          <button class="btn op">Débuter</button>
         </div>
       </div>
 
-      <section class="creation">
+      <section id="creation">
         <div class="container-grid">
           <div class="row">
             <div class="col-md-5 col-xs-12">
@@ -73,7 +74,7 @@
         </div>
       </section>
 
-      <section class="community border-landing">
+      <section id="community" class="border-landing">
         <div class="container-grid">
           <div class="row">
             <div class="col-md-6 col-xs-12">
@@ -88,10 +89,10 @@
           </div>
         </div>
       </section>
-      <section class="contact">
+      <section id="contact">
         <div class="container-grid">
-          <h2>Contactez-nous</h2>
           <form action="" method="POST">
+            <h2>Contactez-nous</h2>
             <div>
               <label for="titre-contact">Titre</label>
               <input type="text" id="titre-contact">
@@ -100,30 +101,27 @@
               <label for="message-contact">Message</label>
               <textarea id="message-contact"></textarea>
             </div> 
-            <div>
-              <input type="submit" value="Envoyer">
+            <div class="text-right">
+              <input type="submit" value="Envoyer" class="btn">
             </div>         
           </form>
         </div>
       </section>
-
-      <div id="creation"></div>
-      <div id="community"></div>
       <footer>
         <img src="assets/img/logo-creative-drawer.png" alt="Logo Creative Drawer" height="64" id="logo-footer">	
         <nav id="menu-footer" class="hidden-xs hidden-sm">
           <ul>
             <li>
-              <a href="#">Fonctionnalités</a>
+              <a href="#band">A propos</a>
             </li>
             <li>
-              <a href="#">Websites</a>
+              <a href="#creation">Fonctionnalités</a>
             </li>
             <li>
-              <a href="#">A propos</a>
+              <a href="#community">Communauté</a>
             </li>
             <li>
-              <a href="#">Contact</a>
+              <a href="#contact">Contact</a>
             </li>	
           </ul>
         </nav>
