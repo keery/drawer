4ff0f3c0ac55ab2edc3e70f579e436a419e62c9e
ESNAULT-Guillaume <mon-compte1131@live.fr>
2018-02-19 17:30:05 +0100

    Responsive page dashboard

--- tpl/dashboard.view.php
+++ tpl/dashboard.view.php
@@ -1,59 +1,66 @@
 <img src="assets/img/speed-picto.svg" alt="" class="picto-page">
 <h1>Dashboard</h1>
-<div id="dashboard">
-  <div class="top">
-    <section>
-      <header>
-        <span>derniers articles</span>
-        <img src="" alt="">
-      </header>
-      <div class="content">
-        <div class="children">
-          <h3>Jean louis</h3>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-          Sunt quo quae repellendus obcaecati maiores? Possimus aut ea mollitia voluptas, 
-          ullam quam labore obcaecati, in error ducimus illo expedita recusandae. Est.</p>
-          <span>il y a 2 jours</span>
-        </div>
-        <div class="children">
-          <h3>Jean louis</h3>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-          Sunt quo quae repellendus obcaecati maiores? Possimus aut ea mollitia voluptas, 
-          ullam quam labore obcaecati, in error ducimus illo expedita recusandae. Est.</p>
-          <span>il y a 2 jours</span>
+<div id="dashboard" class="container-grid">
+  <div class="top row group">
+    <section class="col-md-6 col-xs-12 spacing-right no-spacing-right-sm no-spacing-right-xs group">
+      <div>
+        <header>
+          <span>derniers articles</span>
+          <img src="" alt="">
+        </header>
+        <div class="content">
+          <div class="children">
+            <h3>Jean louis</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Sunt quo quae repellendus obcaecati maiores? Possimus aut ea mollitia voluptas, 
+            ullam quam labore obcaecati, in error ducimus illo expedita recusandae. Est.</p>
+            <span>il y a 2 jours</span>
+          </div>
+          <div class="children">
+            <h3>Jean louis</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Sunt quo quae repellendus obcaecati maiores? Possimus aut ea mollitia voluptas, 
+            ullam quam labore obcaecati, in error ducimus illo expedita recusandae. Est.</p>
+            <span>il y a 2 jours</span>
+          </div>
         </div>
       </div>
     </section>
-    <section>
-      <header>
-        <span>derniers commentaires</span>
-        <img src="" alt="">
-      </header>
-      <div class="content">
-        <div class="children">
-          <h3>Jean louis</h3>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-          Sunt quo quae repellendus obcaecati maiores? Possimus aut ea mollitia voluptas, 
-          ullam quam labore obcaecati, in error ducimus illo expedita recusandae. Est.</p>
-          <span>il y a 2 jours</span>
-        </div>
-        <div class="children">
-          <h3>Jean louis</h3>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-          Sunt quo quae repellendus obcaecati maiores? Possimus aut ea mollitia voluptas, 
-          ullam quam labore obcaecati, in error ducimus illo expedita recusandae. Est.</p>
-          <span>il y a 2 jours</span>
-        </div>
+    <section class="col-md-6 col-xs-12 spacing-left no-spacing-left-sm no-spacing-left-xs group">
+      <div>
+        <header>
+          <span>derniers commentaires</span>
+          <img src="" alt="">
+        </header>
+        <div class="content">
+          <div class="children">
+            <h3>Jean louis</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Sunt quo quae repellendus obcaecati maiores? Possimus aut ea mollitia voluptas, 
+            ullam quam labore obcaecati, in error ducimus illo expedita recusandae. Est.</p>
+            <span>il y a 2 jours</span>
+          </div>
+          <div class="children">
+            <h3>Jean louis</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Sunt quo quae repellendus obcaecati maiores? Possimus aut ea mollitia voluptas, 
+            ullam quam labore obcaecati, in error ducimus illo expedita recusandae. Est.</p>
+            <span>il y a 2 jours</span>
+          </div>
+       </div>
       </div>
     </section>
   </div>
   <hr class="lineSeparator">
-  <div class="bottom">
-    <div class="left">
-      <img src="assets/img/stats.svg" alt="">
+  <div class="bottom row">
+    <div class="left group col-md-7 col-xs-12 spacing-right">
+      <div>
+        <img src="assets/img/stats.svg" alt="">
+      </div>
       <span>TOTAL - 12 300 views</span>
     </div>
-    <img src="assets/img/stats2.svg" alt="">
-   
+    <div class="col-md-5 col-xs-12">
+      <img src="assets/img/stats2.svg" alt="">   
+    </div>
   </div>
 </div>
