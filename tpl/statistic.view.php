6b24242ea5b7d54ea215bc3a3f677a6f540e20d6
Alex <alexandre.charlot@wisembly.com>
2018-03-17 16:36:25 +0100

    fixing stats page

--- tpl/statistic.view.php
+++ tpl/statistic.view.php
@@ -1,29 +1,51 @@
 <img src="assets/img/stat-picto.svg" alt="speed" class="picto-page">
 <h1>Statistiques</h1>
 
-<nav class="container ctn-nav">
-  <div class="nav-links" data-selected-filter="Traffic">
-    <a href="" class="selected">Traffic</a>
-    <a href="">Insights</a>
-  </div>
-  <div class="nav-options cf">
-    <div><a href="" class="selected">Days</a></div>
-    <div><a href="">Weeks</a></div>
-    <div><a href="">Months</a></div>
-    <div><a href="">Years</a></div>
-  </div>
-</nav>
-<br>
-<br>
-<header>
-<img src="assets/img/statistic-picto.svg" width="100%" alt="">
-</header>
-
-<div class="text-center switch-date">
-  <img class="chevron-gauche" src="assets/img/leftArrow.svg" alt="">
-    <div class="stat-date">
-<p id="upper-text">Stats for January 27, 2018</p>
-<p id="bottom-text">Last update: 8:14 PM</p>
+<div id="stats">
+  <nav class="container ctn-nav">
+    <div class="nav-links" data-selected-filter="Traffic">
+      <a href="" class="selected">Traffic</a>
+      <a href="">Insights</a>
     </div>
-    <img class="chevron-droit" src="assets/img/rightArrow.svg" alt="">
+    <div class="nav-options cf">
+      <div><a href="" class="selected">Days</a></div>
+      <div><a href="">Weeks</a></div>
+      <div><a href="">Months</a></div>
+      <div><a href="">Years</a></div>
+    </div>
+  </nav>
+
+  <div class="calendar container container-grid">
+    <div class="row">
+      <img class="u-mw" src="assets/img/statsPicto.svg" alt="">
+      <div class="stats-info col-lg-3">
+        <img src="assets/img/eye.svg" alt="">
+        <span class="title">views</span>
+        <span class="content">1000</span>
+      </div>
+      <div class="stats-info col-lg-3">
+        <img src="assets/img/man-user.svg" alt="">
+        <span class="title">visitors</span>
+        <span class="content">174</span>
+      </div>
+      <div class="stats-info col-lg-3">
+        <img src="assets/img/star.svg" alt="">
+        <span class="title">likes</span>
+        <span class="content">10</span>
+      </div>
+      <div class="stats-info col-lg-3">
+        <img src="assets/img/black-bubble-speech.svg" alt="">
+        <span class="title">comments</span>
+        <span class="content">2387</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="u-d--ib u-tac switch-date u-mw u-mgt--l">
+    <div class="switch-date--content">
+      <p class="date">Stats for January 27, 2018</p>
+      <p class="date-update">Last update: 8:14 PM</p>
+    </div>
+  </div>
+
 </div>
