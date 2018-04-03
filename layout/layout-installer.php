5efdae56856a5f2a7c146f5dc53a6b0d717f6550
Alex <alexandre.charlot@wisembly.com>
2018-04-03 16:20:51 +0200

    adding form

--- layout/layout-installer.php
+++ layout/layout-installer.php
@@ -3,13 +3,31 @@
 	<head lang="fr">
 		<link rel="stylesheet" href="assets/css/dist/style.css">
 		<meta charset="UTF-8">
-		<title>Creative </title>	
+		<title>Creative</title>	
 		<base href="/">
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
 	</head>
 	<body>	
-	<div id="installer">
-    <h1>test</h1>
+	<div id="installer" class="u-pd--m">
+    <form action="" class="form u-pd--l container-grid">
+      <div>
+        <h1 class="u-mgt--0">Database</h1>
+        <input type="text" class="input u-mw u-mgb--m" placeholder='Name'>
+        <input type="text" class="input u-mw u-mgb--m" placeholder='Host'>
+        <input type="text" class="input u-mw u-mgb--m" placeholder='User'>
+        <input type="text" class="input u-mw u-mgb--m" placeholder='Password'>
+        <input type="text" class="input u-mw u-mgb--m" placeholder='Website title'>
+      </div>
+      <div>
+        <h1>Your profile</h1>
+        <input type="text" class="input u-mw u-mgb--m" placeholder='Name'>
+        <input type="text" class="input u-mw u-mgb--m" placeholder='Last name'>
+        <input type="text" class="input u-mw u-mgb--m" placeholder='Email'>
+        <input type="text" class="input u-mw u-mgb--m" placeholder='Password'>
+        <input type="text" class="input u-mw u-mgb--m" placeholder='Confirm password'>
+        <input type="submit" value="Submit" class="button u-mw btn-validate">
+      </div> 
+    </form>
 	</div>
 	<script src="http://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
 	<script src="js/index.js"></script>	
