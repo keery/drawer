ab4bff974dc94b4825f46614b7ecd8de0a1ee499
louisdeluche <louis87@hotmail.fr>
2018-05-29 12:21:07 +0200

    tempo template

--- tpl/backend/template.view.php
+++ tpl/backend/template.view.php
@@ -1,24 +1,50 @@
+<?php
+use Module\Entity\InstallerConfig;
+
+$installerConfig = new InstallerConfig();
+$config = $installerConfig->configFormAdd();
+$errors = [];
+
+if (!empty($_POST)) {
+    $installerConfig->addConfig($_POST);
+}
+
+include "module/view/form/form.frm.php";
+?>
+
 <img src="assets/img/template-brush-black.svg" alt="" class="picto-page">
-<h1>Template</h1>
+<h1>Design</h1>
 <div id="settings">
-  <section class="container padding-box">
-    <div class="container-grid">
-      <div class="row">
-        <div class="col-md-3 col-xs-12 u-tac group-xs">
-          <label>Icône du site</label>
-          <div class="preview-icon u-block--center"></div>
-          <div class="text-center">
-            <a href="" class="button">Ajouter</a>
-          </div>
+
+    <h2>Style</h2>
+
+    <section class="container padding-box">
+        <div class="container-grid">
+            <div class="row">
+
+                <div class="col-md-6 col-xs-12 group spacing-left">
+                    <label>Couleur Principal</label>
+                    <input type="color" name="favcolor" value="#304A52">
+                </div>
+                <div class="col-md-6 col-xs-12 group spacing-left">
+                    <label>Couleur Secondaire</label>
+                    <input type="color" name="favcolor" value="#379085">
+                </div>
+                <div class="col-md-6 col-xs-12 group spacing-left">
+                    <label>Couleur sous-titre</label>
+                    <input type="color" name="favcolor" value="#FFFFFF">
+                </div><div class="col-md-6 col-xs-12 group spacing-left">
+                    <label>Couleur sous-titre secondaire</label>
+                    <input type="color" name="favcolor" value="#D5DCE3">
+                </div>
+                <div class="col-md-6 col-xs-12 group spacing-left">
+                    <label>Couleur Texte </label>
+                    <input type="color" name="favcolor" value="#304A52">
+                </div>
+                <div class="text-right text-center-xs col-md-3 col-xs-12">
+                    <input type="submit" value="Enregistrer" class="button btn-validate">
+                </div>
+            </div>
         </div>
-        <div class="col-md-6 col-xs-12 group spacing-left">
-          <label>Titre du site</label>
-          <input type="text" class="input">
-        </div>
-        <div class="text-right text-center-xs col-md-3 col-xs-12">
-          <input type="submit" value="Enregistrer" class="button btn-validate">          
-        </div>
-      </div>
-    </div>
-  </section>
+    </section>
 </div>
