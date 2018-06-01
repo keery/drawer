cd01cfb0a89c0c61c1758fc89b57c426baeb89ac
Hassan AKAR <hassan.akar@outlook.fr>
2018-06-01 11:42:11 +0200

    .

--- controllers/ImageController.php
+++ controllers/ImageController.php
@@ -48,19 +48,4 @@ class ImageController {
 
 
 
-    public function deleteAction($params)
-    {
-        if(isset($params['id'])) $image = Image::findOneBy(array('id' => $params['id']));
-        else $image = new Image();
-
-        if(empty($image)) {
-            throw new Erreur("L'image contenant l'id ".$params['id']." n'existe pas");
-            return false;
-        }
-
-        View::render("backend/image-delete.view.php", 'layout.php',array(
-            "image" => $image
-        ));
-    }
-
 }
---
+++ tpl/backend/image-detail.view.php
@@ -0,0 +1,61 @@
+<img src="assets/img/article-picto.svg" alt="" class="picto-page">
+<h1>Comment améliorer son dessin en 3 semaines</h1>
+<?php $form->form_head(); ?>
+<section class="container group">
+    <div class="bloc">
+        <!-- <img src="module/captcha/Captcha.php" /> -->
+        <div class="bloc-titre">
+            <span>Informations</span>
+        </div>
+        <div class="bloc-content">
+            <?php foreach(getNotifs('error') as $notif) : ?>
+                <div class="notif error">
+                    <span class="notif-icone"></span>
+                    <div class="notif-titre">Erreur:</div>
+                    <?php echo $notif; ?>
+                </div>
+            <?php endforeach; ?>
+            <div class="row spacing">
+                <div class="group col-sm-6 col-xs-12">
+                    <?php echo $form->label('titre'); ?>
+                    <?php echo $form->input('titre', ['class' => 'input']); ?>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <label>Catégorie</label>
+                    <?php echo $form->input('categorie', ['class' => 'select']); ?>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <label>État</label>
+                    <?php echo $form->input('active'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="container group">
+    <div class="bloc">
+        <div class="bloc-titre">
+            <span>Contenu</span>
+        </div>
+        <div class="bloc-content">
+            <?php echo $form->input('description', ['class' => 'editor-img']); ?>
+        </div>
+    </div>
+</section>
+
+<section class="container group">
+    <div class="bloc">
+        <div class="bloc-titre">
+            <span>Fichiers</span>
+        </div>
+        <div class="bloc-content">
+            <?php echo $form->input('image'); ?>
+        </div>
+    </div>
+</section>
+<div class="text-right text-center-xs col-xs-12">
+    <a href="<?php echo path('articles'); ?>" class="button btn-validate">Retour à la liste</a>
+    <?php echo $form->input('submit', ['class' => 'button btn-validate']); ?>
+</div>
+<?php $form->form_bottom(); ?>
