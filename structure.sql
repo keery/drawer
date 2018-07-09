3c34cc543784e867edc10631c68af321dd279c12
Hassan AKAR <hassan.akar@outlook.fr>
2018-07-09 19:36:59 +0200

    Structure sql

--- structure.sql
+++ structure.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Client :  127.0.0.1
--- Généré le :  Lun 09 Juillet 2018 à 15:49
--- Version du serveur :  5.7.14
--- Version de PHP :  7.0.10
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -76,12 +67,6 @@ CREATE TABLE `cd_infosite` (
   `id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `cd_page`
---
-
 CREATE TABLE `cd_page` (
   `id` int(11) NOT NULL,
   `id_parent` int(11) DEFAULT NULL,
@@ -93,7 +78,6 @@ CREATE TABLE `cd_page` (
   `date_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'contenu'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 
 
 CREATE TABLE `cd_settings` (
@@ -142,147 +126,76 @@ CREATE TABLE `cd_user_role` (
   `nom` varchar(50) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Index pour les tables exportées
---
 
---
--- Index pour la table `cd_article`
---
 ALTER TABLE `cd_article`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `cd_article_image`
---
 ALTER TABLE `cd_article_image`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `cd_avis`
---
 ALTER TABLE `cd_avis`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `cd_categorie_article`
---
 ALTER TABLE `cd_categorie_article`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `cd_commentaire`
---
 ALTER TABLE `cd_commentaire`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `cd_image`
---
 ALTER TABLE `cd_image`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `cd_infosite`
---
 ALTER TABLE `cd_infosite`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `cd_page`
---
 ALTER TABLE `cd_page`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `cd_settings`
---
 ALTER TABLE `cd_settings`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `cd_user`
---
 ALTER TABLE `cd_user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- Index pour la table `cd_user_article`
---
 ALTER TABLE `cd_user_article`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `cd_user_role`
---
 ALTER TABLE `cd_user_role`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `cd_article`
---
 ALTER TABLE `cd_article`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
---
--- AUTO_INCREMENT pour la table `cd_article_image`
---
+
 ALTER TABLE `cd_article_image`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `cd_avis`
---
+
 ALTER TABLE `cd_avis`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `cd_categorie_article`
---
+
 ALTER TABLE `cd_categorie_article`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=172;
---
--- AUTO_INCREMENT pour la table `cd_commentaire`
---
+
 ALTER TABLE `cd_commentaire`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
---
--- AUTO_INCREMENT pour la table `cd_image`
---
+
 ALTER TABLE `cd_image`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=177;
---
--- AUTO_INCREMENT pour la table `cd_infosite`
---
+
 ALTER TABLE `cd_infosite`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `cd_page`
---
+
 ALTER TABLE `cd_page`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
---
--- AUTO_INCREMENT pour la table `cd_settings`
---
+
 ALTER TABLE `cd_settings`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT pour la table `cd_user`
---
+
 ALTER TABLE `cd_user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT pour la table `cd_user_article`
---
+
 ALTER TABLE `cd_user_article`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT pour la table `cd_user_role`
---
+
 ALTER TABLE `cd_user_role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
