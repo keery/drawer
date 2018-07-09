7a6b90507c34a3603f51ed660b37839216f4c08d
louisdeluche <louis87@hotmail.fr>
2018-07-09 19:16:08 +0200

    fix  if index

--- index.php
+++ index.php
@@ -9,22 +9,23 @@ require(CONF.'functions.php');
 $loader = require(CONF.'autoload.php');
 Autoloader::register();
 
+if( isset($_SESSION[PREFIX."user"]['id']) ) {
+    $expire = date('Y-m-d H:i:s');
+    $user = User::findOneBy(['id' => $_SESSION[PREFIX."user"]['id']]);
+    if((isset($_SESSION[PREFIX."user"]['token']) && $user->getToken() != $_SESSION[PREFIX."user"]['token']) || ($user->getExpire() < $expire) ) session_destroy();
+    $token = uniqid();
+    $_SESSION[PREFIX."user"]['token'] = $token;
+    $user->setToken($token);
+    $user->setExpire(date('Y-m-d H:i:s', strtotime('+4 hour')));
+    $user->save();
+}
+
 
 if (!file_exists(CONF.'config.php')){
     $URI = "installer-config";
 }
 elseif( !User::findOneBy(['role' => "ADMINISTRATEUR"] )){
     $URI = "installer-user";
-}
- elseif( isset($_SESSION[PREFIX."user"]['id']) ) {
-	$expire = date('Y-m-d H:i:s');
-	$user = User::findOneBy(['id' => $_SESSION[PREFIX."user"]['id']]);
-	if((isset($_SESSION[PREFIX."user"]['token']) && $user->getToken() != $_SESSION[PREFIX."user"]['token']) || ($user->getExpire() < $expire) ) session_destroy();
-	$token = uniqid();
-	$_SESSION[PREFIX."user"]['token'] = $token;
-	$user->setToken($token);
-	$user->setExpire(date('Y-m-d H:i:s', strtotime('+4 hour')));
-	$user->save();
 }
 
 else{
