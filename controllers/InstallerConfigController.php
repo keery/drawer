d534cb504b52bf8fb7ced6c57e2a13a74ef01963
Hassan AKAR <hassan.akar@outlook.fr>
2018-07-09 19:26:07 +0200

    Test BDD installer

--- controllers/InstallerConfigController.php
+++ controllers/InstallerConfigController.php
@@ -24,18 +24,26 @@ class InstallerConfigController {
                 $errors = $validate->checkForm($config, $params);
 
                 if(empty($errors)){
+                    try {
+                        $this->pdo = new \PDO('mysql:host='.$params['host'].';dbname='.$params['databasename'], $params['user'], $params['pwd']);
 
-                    $txtconfi= '<?php
-                 define("TITLE", "'.$params['websitetitle'].'");
-                 define("HOST", "'.$params['host'].'");
-                 define("DB_NAME", "'.$params['databasename'].'");
-                 define("USER", "'.$params['user'].'");
-                 define("PASS", "'.$params['pwd'].'");
 
-				';
+                            $txtconfi= '<?php
+                        define("TITLE", "'.$params['websitetitle'].'");
+                        define("HOST", "'.$params['host'].'");
+                        define("DB_NAME", "'.$params['databasename'].'");
+                        define("USER", "'.$params['user'].'");
+                        define("PASS", "'.$params['pwd'].'");
 
-                    file_put_contents(CONF.'config.php', $txtconfi, FILE_APPEND | LOCK_EX);
-                    redirectToRoute("installer-user");
+                        ';
+
+                        file_put_contents(CONF.'config.php', $txtconfi, FILE_APPEND | LOCK_EX);
+                        redirectToRoute("installer-user");
+                    }
+                    catch(PDOException $e) {
+                        throw new Erreur("Connexion à la base de donnée impossible");
+                        return false;
+                    }
                 }
                 else $data['errors'] = $errors;
             }
