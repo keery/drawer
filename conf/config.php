c5c4669f25838ba6c8781308642909b837cc0194
louisdeluche <32841134+louisdeluche@users.noreply.github.com>
2018-07-11 11:40:59 +0200

    Delete config.php

--- conf/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-                        define("TITLE", "Mon site ");
-                        define("HOST", "localhost");
-                        define("DB_NAME", "louis");
-                        define("USER", "root");
-                        define("PASS", ""); 
